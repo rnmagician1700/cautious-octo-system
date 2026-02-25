65d40b50577315ce4643edcf28f0d8898511c1a7
rnmagician1700 <ramya17neel06@gmail.com>
2026-02-25 16:55:33 +0530

    Delete task1.py

--- task1.py
+++
@@ -1,8 +0,0 @@
-n1 = float(input("Enter the first number: "))
-n2 = float(input("Enter the second number: "))
-
-print("\n")
-print("Addition: ", n1 + n2)
-print("Subtraction: ", n1 - n2)
-print("Multiplication: ", n1 * n2)
-print("Division: ", n1 / n2)
